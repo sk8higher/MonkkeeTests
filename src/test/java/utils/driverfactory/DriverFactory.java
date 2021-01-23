540dc9a89412ca217d0fc942369b7d5a90463946
Nelly Simkova <sk8higher@iballwasrawt.ru>
2021-01-23 14:48:40 +0300

    добавлена DriverFactory

---
+++ src/test/java/utils/driverfactory/DriverFactory.java
@@ -0,0 +1,32 @@
+package utils.driverfactory;
+
+import io.github.bonigarcia.wdm.config.DriverManagerType;
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import io.github.bonigarcia.wdm.managers.FirefoxDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class DriverFactory {
+    private static WebDriver driver;
+
+    public static WebDriver getDriver(String browserType) {
+        switch (browserType) {
+            case "Chrome":
+                ChromeDriverManager.getInstance(DriverManagerType.CHROME).setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--no-sandbox", "--headless", "--disable-gpu");
+                driver = new ChromeDriver(chromeOptions);
+                break;
+            case "Firefox":
+                FirefoxDriverManager.getInstance(DriverManagerType.FIREFOX).setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.setHeadless(true);
+                driver = new FirefoxDriver(firefoxOptions);
+                break;
+        }
+        return driver;
+    }
+}
