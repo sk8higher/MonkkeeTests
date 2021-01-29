c62fb5367d65482a57bc5980411be9ceee278e80
Nelly Simkova <sk8higher@iballwasrawt.ru>
2021-01-29 17:13:05 +0300

    добавлено логирование

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,19 +1,23 @@
 package tests;
 
+import lombok.extern.log4j.Log4j2;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 
+@Log4j2
 public class LoginTest extends BaseTest {
     private LoginPage loginPage;
 
     @BeforeMethod
     public void beforeMethod() {
         driver.get("https://my.monkkee.com/#/");
+
+        log.info("Opened the page");
     }
 
-    @Test(description = "Login test")
+    @Test(description = "Login test", priority = 0)
     public void loginTest() {
         loginPage = new LoginPage(driver);
 
@@ -21,6 +25,8 @@ public class LoginTest extends BaseTest {
                 .enterPassword()
                 .login()
                 .checkDonationAlert();
+
+        log.info("Tried to log in");
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://my.monkkee.com/#/entries");
     }
