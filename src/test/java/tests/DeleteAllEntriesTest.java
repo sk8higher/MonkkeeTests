ec68b6340b27ca3c38f3ee6c15ebbffe182f5c18
sk8higher <sk8higher@iballwasrawt.ru>
2021-02-01 20:31:38 +0300

    добавила новые тесты

---
+++ src/test/java/tests/DeleteAllEntriesTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import lombok.extern.log4j.Log4j2;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.EntriesPage;
+
+@Log4j2
+public class DeleteAllEntriesTest extends BaseTest {
+    private EntriesPage entriesPage;
+
+    @Test(description = "Delete all entries", groups = {"entrypage"}, priority = 1)
+    public void deleteAllEntriesTest() {
+        entriesPage = new EntriesPage(driver);
+
+        entriesPage.login()
+                .deleteAllEntries();
+
+        Assert.assertTrue(entriesPage.isThereNoEntries());
+    }
+}
