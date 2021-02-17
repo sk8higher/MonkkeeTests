e17028034753e0735c067743d8ec638b26aac248
sk8higher <sk8higher@iballwasrawt.ru>
2021-02-17 21:32:36 +0300

    добавлен тест

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
+
 
 @Log4j2
 public class LoginPage extends BasePage {
@@ -21,6 +23,9 @@ public class LoginPage extends BasePage {
 
     @FindBy(xpath = "//*[@type='submit']")
     private WebElement loginButton;
+
+    @FindBy(xpath = "(//a[@class='footer-link'])[2]")
+    private WebElement supportButton;
 
     public LoginPage(WebDriver driver) {
         super(driver);
@@ -77,5 +82,25 @@ public class LoginPage extends BasePage {
         }
 
         return this;
+    }
+
+    public LoginPage goToSupportPage() {
+        supportButton.click();
+        log.info("Clicked Support button");
+        return this;
+    }
+
+    public LoginPage switchToAnotherTab() {
+        ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1));
+        log.info("Switched to another tab");
+        return this;
+    }
+
+    public boolean isSupportParagraphVisible() {
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        WebElement paragraph = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[text()[normalize-space()='Support']]")));
+        log.info("Found a Support paragraph");
+        return paragraph.isDisplayed();
     }
 }
